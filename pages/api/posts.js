f974181da51c0c267f8fbf6f40b05e9a083a5f0d
GitHubのユーザー名 <>
2026-05-13 21:36:02 +0900

    fix private key handling

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   try {
     const auth = new google.auth.JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
       scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     });
 
